9866a7f256818f01ccd61340f451dc9e9367f5a5
sohailB <bhattisohail20@gmail.com>
2023-12-18 15:12:12 +0500

    message

--- index.js
+++ index.js
@@ -111,6 +111,7 @@ app.post('/api/download-pdf', async (req, res) => {
     try {
         console.log("initializing")
         const url = req.body.url
+        console.log("req.body", req.body)
         if (!url) return res.status(404).send({ message: "url not found" })
         const browser = await puppeteer.launch({ headless: 'new', executablePath: '/usr/bin/chromium-browser' });
         // const browser = await puppeteer.launch({ headless: 'new' });
